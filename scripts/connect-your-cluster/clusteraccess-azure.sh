44acd23db0d50fd9c12cb1deb473a7c10cb7aa0f
Fernando Silva <fernandodd66@gmail.com>
2024-12-05 15:10:10 +0000

    test

--- scripts/connect-your-cluster/clusteraccess-azure.sh
+++ scripts/connect-your-cluster/clusteraccess-azure.sh
@@ -1,18 +1,20 @@
 # These steps reiterate the commands seen at https://developer.humanitec.com/integration-and-extensions/containerization/kubernetes/#2-configure-aks-cluster-access
 
-export AKS_CLUSTER_ID=$(az aks show -n Quickstart-AKS-749a92 -g Quickstart-AKS-749a92 --query id -o tsv)
+export AKS_CLUSTER_ID=$(az aks show -n "Quickstart-AKS-749a92" -g "Quickstart-AKS-749a92" --query id -o tsv)
 
 # Assign required roles for cluster access with AKS-managed Microsoft Entra integration
 export MANAGED_IDENTITY_PRINCIPAL_ID=$(az identity show \
-  -n quickstart-azure-cloudaccount -g Q \
+  -n quickstart-azure-cloudaccount -g Quickstart-AKS-749a92 \
   --query principalId -o tsv)
 
 az role assignment create \
   --role "Azure Kubernetes Service Cluster User Role" \
   --scope ${AKS_CLUSTER_ID} \
-  --assignee ${MANAGED_IDENTITY_PRINCIPAL_ID}
+  --assignee-object-id "473b40b0-190e-406d-9319-4021de8cab62" \
+  --assignee-principal-type "ServicePrincipal"
 
 az role assignment create \
   --role "Azure Kubernetes Service RBAC Cluster Admin" \
   --scope ${AKS_CLUSTER_ID} \
-  --assignee ${MANAGED_IDENTITY_PRINCIPAL_ID}
+  --assignee-object-id "473b40b0-190e-406d-9319-4021de8cab62" \
+  --assignee-principal-type "ServicePrincipal"
--- scripts/connect-your-cluster/create-resdef-azure.sh
+++ scripts/connect-your-cluster/create-resdef-azure.sh
@@ -5,19 +5,19 @@ cat << EOF > resdef-azure.yaml
 apiVersion: entity.humanitec.io/v1b1
 kind: Definition
 metadata:
-  id: ${CLOUD}-quickstart
+  id: azure-quickstart
 entity:
-  name: ${CLOUD}-quickstart
+  name: azure-quickstart
   type: k8s-cluster
   # The driver_account references a Cloud Account of type "azure-identity"
   # which needs to be configured for your Organization.
-  driver_account: ${CLOUD_ACCOUNT_ID}
+  driver_account: quickstart-azure
   driver_type: humanitec/k8s-cluster-aks
   driver_inputs:
     values:
-      name: ${AKS_CLUSTER_NAME}
-      resource_group: ${AKS_RESOURCE_GROUP_NAME}
-      subscription_id: ${SUBSCRIPTION_ID}
+      name: Quickstart-AKS-749a92
+      resource_group: Quickstart-AKS-749a92
+      subscription_id: 84c846a7-3273-4925-be8c-a43f407fa842
       # Add this exact server_app_id for a cluster using AKS-managed Entra ID integration
       server_app_id: 6dae42f8-4368-4678-94ff-3960e28e3630
   criteria:
