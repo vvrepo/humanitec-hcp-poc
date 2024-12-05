b42a038fe65359e4822bf94ebc95544f9726a460
Fernando Silva <fernandodd66@gmail.com>
2024-12-05 15:02:46 +0000

    test

--- scripts/connect-your-cluster/clusteraccess-azure.sh
+++ scripts/connect-your-cluster/clusteraccess-azure.sh
@@ -1,10 +1,10 @@
 # These steps reiterate the commands seen at https://developer.humanitec.com/integration-and-extensions/containerization/kubernetes/#2-configure-aks-cluster-access
 
-export AKS_CLUSTER_ID=$(az aks show -n ${AKS_CLUSTER_NAME} -g ${AKS_RESOURCE_GROUP_NAME} --query id -o tsv)
+export AKS_CLUSTER_ID=$(az aks show -n Quickstart-AKS-749a92 -g Quickstart-AKS-749a92 --query id -o tsv)
 
 # Assign required roles for cluster access with AKS-managed Microsoft Entra integration
 export MANAGED_IDENTITY_PRINCIPAL_ID=$(az identity show \
-  -n ${MANAGED_IDENTITY_NAME} -g ${MANAGED_IDENTITY_RESOURCE_GROUP} \
+  -n quickstart-azure-cloudaccount -g Q \
   --query principalId -o tsv)
 
 az role assignment create \
